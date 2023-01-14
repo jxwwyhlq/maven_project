df40ebdb0161120a08da58daba00e59ca6303872
jiawei <1234@qq.com>
2023-01-14 09:21:59 +0800

    second commit

--- src/main/java/com/itheima/demo.java
+++ src/main/java/com/itheima/demo.java
@@ -3,6 +3,7 @@ package com.itheima;
 public class demo {
     public String say(String name){
         System.out.println("hello"+name);
+        System.out.println("hello"+name);
 
         return "Hello "+name;
     }
